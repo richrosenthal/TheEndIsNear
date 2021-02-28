c317070b16b4bfd5411cbfea6e5acd9dce5eb513
richrosenthal <rrosenthal2013@gmail.com>
2021-02-28 01:19:21 -0500

    create functions for retirement form

--- index.js
+++ index.js
@@ -20,10 +20,72 @@ function createRetirementForm(){
    Current Savings: <input type="text" id="currentSavings">
    Retirement Age: <input type="text" id="retirementAge">
    Monthly Contribution: <input type="text" id="monthlyContributions">
+   <input type="submit" value="Calculate Morty">
    </form>
 
    `
-   // requirementForm.addEventListener("submit", retirementFormSubmission)
-
+   retirementForm.addEventListener("submit", retirementFormSubmission)
 
 }
+
+function retirementFormSubmission(){
+   event.preventDefault();
+   let currentAge = document.getElementById("currentAge").value
+   let currentSavings = document.getElementById("currentSavings").value
+   let retirementAge = document.getElementById("retirementAge").value
+   let monthlyContributions = document.getElementById("monthlyContributions").value
+
+   console.log("I'm here in the form submission function")
+   console.log(currentAge)
+   console.log(retirementAge + 1)
+
+   let retirementObject = {
+     currentAge: currentAge,
+     currentSavings: currentSavings,
+     retirementAge: retirementAge,
+     monthlyContributions: monthlyContributions,
+   }
+
+  
+
+  let finalResults = document.getElementById("results-container")
+
+   finalResults.innerHTML +=
+
+   `
+    <h1>HERE'S THE RESULTS MORTY! </h1>
+   `
+
+}
+
+// function equipmentFormSubmission(){
+//   event.preventDefault();
+//   let name = document.getElementById("name").value
+//   let description = document.getElementById("description").value
+//   let location = document.getElementById("location").value
+//   let department = document.getElementById("department").value
+//   let task_description = document.getElementById("task_description").value
+//
+//   let equipment = {
+//     name: name,
+//     description: description,
+//     location: location,
+//     department: department,
+//
+//   }
+//
+//   fetch(`${BASE_URL}/equipment`, {
+//     method: "POST",
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(equipment)
+//   })
+//   .then(resp =>  resp.json())
+//   .then (equipment => {
+//     let e = new Equipment(equipment.id, equipment.name,
+//       equipment.description, equipment.location, equipment.department)
+//     e.renderEquipment();
+//   })
+// }
